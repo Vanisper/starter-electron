a4a72f9e6102bbd128fed361beac70ac6423dbc2
Vanisper <273266469@qq.com>
2025-11-06 16:25:15 +0800

    chore: configure aliasOptions

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type AliasOptions } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import electron from "vite-plugin-electron";
+import { fileURLToPath } from 'node:url';
+
+const alias: AliasOptions = {
+  "@": fileURLToPath(new URL("./src", import.meta.url)),
+}
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
+  resolve: { alias },
   plugins: [
     vue(),
     electron([
       {
         entry: 'src/electron/main/index.ts',
         vite: {
-          build: { outDir: 'dist-electron/main' }
+          build: { outDir: 'dist-electron/main' },
+          resolve: { alias },
         }
       },
       {
         entry: 'src/electron/preload/index.ts',
         vite: {
-          build: { outDir: 'dist-electron/preload' }
+          build: { outDir: 'dist-electron/preload' },
+          resolve: { alias },
         }
       },
     ]),
